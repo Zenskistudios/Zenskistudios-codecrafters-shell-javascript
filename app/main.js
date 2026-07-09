9a00ef2c714496d63d1de21f54e3d553c45402d9
Zenskistudios <jegedejedidiah@gmail.com>
2026-07-09 08:27:48 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -15,6 +15,9 @@ startShell();
 rl.on("line", (command) => {
   if (command === "exit") {
     rl.close();
+  } else if (command.startsWith("echo ")) {
+    console.log(command.slice(5));
+    startShell();
   } else {
     console.log(`${command}: command not found`);
     startShell();
